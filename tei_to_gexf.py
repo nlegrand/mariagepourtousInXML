bc647b8674fbfd133bc4ef2237251ae36e35c9eb
Nicolas Legrand <nicolas.legrand@gmail.com>
2013-03-11 23:58:02 +0100

    Make same scale for edges and nodes

--- tei_to_gexf.py
+++ tei_to_gexf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import xml.etree.ElementTree as ET
-import hashlib, json, sys
+import hashlib, json, sys, xml.dom.minidom
 
 nodes = {}
 edges = {}
@@ -28,8 +28,8 @@ for xml_file_name in sys.argv[1:]:
             elif last_intervention['id_intervenant'] not in edges[id_intervenant]:
                 edges[id_intervenant][last_intervention['id_intervenant']] = 1.0
             else:
-                edges[id_intervenant][last_intervention['id_intervenant']] += 1.0
-                nodes[id_intervenant]['size'] += 1.0
+                edges[id_intervenant][last_intervention['id_intervenant']] += 0.3
+                nodes[id_intervenant]['size'] += 0.3
 
 #print json.dumps(nodes)
 
@@ -51,8 +51,8 @@ for key in nodes:
     if 'n' in nodes[key]:
         node = ET.SubElement(gexf_nodes,'node')
         node.set("id", key)
-        if nodes[key]['size'] > 50:
-            node.set("label", nodes[key]['who'])
+#        if nodes[key]['size'] > 50:
+        node.set("label", nodes[key]['who'])
         viz_size =  ET.SubElement(node, 'viz:size')
         viz_size.set('value', str(nodes[key]['size']))
         if nodes[key]['n'] == 'GDR':
@@ -95,14 +95,16 @@ i = 0
 for source in edges:
     if source not in bad_nodes:
         for target in edges[source]:
-            if target not in bad_nodes:
+            if target not in bad_nodes and target != source:
                 edge = ET.SubElement(gexf_edges, "edge")
                 edge.set("id", str(i))
                 edge.set("source", source)
                 edge.set("target", target)
+                edge.set("weight", str(edges[source][target]))
                 viz_size = ET.SubElement(edge, 'viz:size')
                 viz_size.set("value", str(edges[source][target]))
                 i += 1
         
 
-ET.dump(gexf)
+xml = xml.dom.minidom.parseString(ET.tostring(gexf, encoding="utf-8", method="xml"))
+print xml.toprettyxml(indent="  ", newl="\n", encoding="utf-8")
